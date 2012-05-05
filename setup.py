61512d0599aa90eecbf67e1c53505cbf4e7f0d31
Sam Rushing <gitsam@rushing.nightmare.com>
2012-05-05 14:42:49 -0700

    tweaking the download url to get pip to work

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup (
     license      = "Simplified BSD",
     keywords     = "zlib streaming generator",
     url          = 'http://github.com/samrushing/zstream/',
-    download_url = "http://github.com/samrushing/zstream/tarball/v0.1#egg=zstream",
+    # pip demands the project-version in the #egg= section
+    download_url = "http://github.com/samrushing/zstream/tarball/v0.1#egg=zstream-0.1",
     ext_modules  = ext_modules,
     cmdclass     = {'build_ext': build_ext}
     )
